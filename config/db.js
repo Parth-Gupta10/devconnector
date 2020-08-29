2d551b0c16e9f9513835547b845d6d5c820d3ce1
Parth Gupta <parth1716@gmail.com>
2020-08-29 20:28:26 +0530

    connected to mongodb atlas

---
+++ config/db.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+// const connectDB = () => {
+//   mongoose.connect(db)
+//   .then(() => {
+//     console.log("MongoDB connected");
+//   })
+//   .catch(err => console.log(err));
+// }
+
+// Although we can use then and catch as mongoose commands returns promises but I will be using
+// async await function for keeping the code same as that taught in course. Promise version has
+// been written above in comments if I would want to refer in future.
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true}) //these options passed to clear deprecation warnings
+    console.log('Mongoose connected');
+  } catch (err) {
+    console.log(err);
+    //Exit process with failure
+    process.exit(1);
+  }
+}
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
-const PORT = process.env.PORT || 5000; //used process.env.PORT for heroku
+//Connect Database
+connectDB();
 
 app.get("/", (req, res) => {
   res.send('Api running');
 })
 
+const PORT = process.env.PORT || 5000; //used process.env.PORT for heroku
 app.listen(PORT, () => {
   console.log("Server started on port " + PORT);
 });
