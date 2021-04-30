a405e1c9958d021c3f42163f04c762ea6bc3caaf
Parth Gupta <parth1716@gmail.com>
2021-04-30 14:10:39 +0530

    add redux store boilerplate

--- client/src/App.js
+++ client/src/App.js
@@ -5,19 +5,23 @@ import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import './App.css';
+//Redux
+import {Provider} from 'react-redux';
+import store from './store'
 
 const App = () =>
-  <Router>
-    <Fragment>
-      <Navbar />
-      <Route exact path='/' component={Landing} />
-      <section className='container'>
-        <Switch>
-          <Route exact path='/register' component={Register} />
-          <Route exact path='/login' component={Login} />
-        </Switch>
-      </section>
-    </Fragment>
-  </Router>
-
+  <Provider store={store}>
+    <Router>
+        <Fragment>
+          <Navbar />
+          <Route exact path='/' component={Landing} />
+          <section className='container'>
+            <Switch>
+              <Route exact path='/register' component={Register} />
+              <Route exact path='/login' component={Login} />
+            </Switch>
+          </section>
+        </Fragment>
+      </Router>
+  </Provider>
 export default App;
