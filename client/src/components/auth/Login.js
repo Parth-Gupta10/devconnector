f8597dd89cbad3948acb5a4cf3ac2bab35218a4b
Parth Gupta <parth1716@gmail.com>
2021-04-30 13:33:04 +0530

    add login and register page

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,10 +1,61 @@
-import React from 'react'
+import React, { Fragment, useState } from 'react';
+import {Link} from 'react-router-dom'
 
 const Login = () => {
+
+    const [formData, setFormData] = useState({
+        email: '',
+        password: ''
+      });
+    
+      const { email, password } = formData;
+    
+      const onChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+      }
+
+      const onSubmit = (e) => {
+        e.preventDefault();
+        console.log(formData);
+      }
+        
     return (
-        <div>
-            login shit
-        </div>
+        <Fragment>
+            <h1 className="large text-primary">Sign In</h1>
+            <p className="lead"><i className="fas fa-user"></i> Sign In Your Account</p>
+
+            <form className="form" onSubmit={e => onSubmit(e)}>
+                
+                <div className="form-group">
+                    <input 
+                        type="email" 
+                        placeholder="Email Address" 
+                        value={email}
+                        onChange={e => onChange(e)}
+                        name="email" 
+                        required />
+                    
+                </div>
+                <div className="form-group">
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        name="password"
+                        value={password}
+                        onChange={e => onChange(e)}
+                        minLength="6"
+                    />
+                </div>
+               
+                <input 
+                    type="submit" 
+                    className="btn btn-primary" 
+                    value="Login" />
+            </form>
+            <p className="my-1">
+                Don't have an account? <Link to="/register">Sign In</Link>
+            </p>
+        </Fragment>
     )
 }
 
