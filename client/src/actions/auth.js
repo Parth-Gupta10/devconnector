71b0e342b95c205fee2d2c08c7ff3f659b6f297c
Parth Gupta <parth1716@gmail.com>
2021-05-02 19:40:06 +0530

    add basic dashboard and create profile

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,4 +1,4 @@
-import { REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR, LOGIN_SUCCESS, LOGIN_FAIL, LOGOUT } from './types';
+import { REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR, LOGIN_SUCCESS, LOGIN_FAIL, LOGOUT, CLEAR_PROFILE } from './types';
 import axios from 'axios'
 //to show errors while registering
 import setAlert from './alert';
@@ -97,6 +97,9 @@ export const login = (email, password) => async dispatch => {
 
 //logout user and clear profile
 export const logout = () => dispatch => {
+    dispatch({
+        type: CLEAR_PROFILE
+    })
     dispatch({
         type: LOGOUT
     })
---
+++ client/src/reducers/profile.js
@@ -0,0 +1,40 @@
+import { GET_PROFILE, PROFILE_ERROR, CLEAR_PROFILE } from '../actions/types';
+
+const initialState = {
+    profile: null,
+    profiles: [],
+    repos: [],
+    loading: true,
+    error: {}
+};
+
+function profileReducer(state = initialState, action) {
+    const { type, payload } = action;
+
+    switch (type) {
+        case GET_PROFILE:
+            return {
+                ...state,
+                profile: payload,
+                loading: false
+            };
+
+        case PROFILE_ERROR:
+            return {
+                ...state,
+                error: payload,
+                loading: false,
+                profile: null
+            };
+        case CLEAR_PROFILE:
+            return {
+                ...state,
+                profile: null,
+                repos: []
+            };
+        default:
+            return state;
+    }
+}
+
+export default profileReducer;
