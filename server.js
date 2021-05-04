bbe32f6b6d1a1397699068b9f9215a2a7d9bb2a7
Parth Gupta <parth1716@gmail.com>
2021-05-05 04:06:11 +0530

    prepare for deploy

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -7,7 +8,7 @@ const app = express();
 connectDB();
 
 //Middleware
-app.use(express.json({extended: false})); //Now body parser is include in express so instead of using bodyParser.json we use this.
+app.use(express.json({ extended: false })); //Now body parser is include in express so instead of using bodyParser.json we use this.
 
 //Routes
 app.use('/users', require('./routes/users'));
@@ -15,9 +16,15 @@ app.use('/auth', require('./routes/auth'));
 app.use('/profile', require('./routes/profile'));
 app.use('/posts', require('./routes/posts'));
 
-app.get("/", (req, res) => {
-  res.send('Api running');
-})
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 
 const PORT = process.env.PORT || 5000; //used process.env.PORT for heroku
 app.listen(PORT, () => {
